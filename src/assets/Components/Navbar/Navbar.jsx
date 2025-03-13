b829f86abff1f9d62b238f2d93a9882fbac2bf61
jnakht <jisan.cse.cu@gmail.com>
2025-03-13 18:59:35 +0600

    navbar added

--- src/assets/Components/Navbar/Navbar.jsx
+++ src/assets/Components/Navbar/Navbar.jsx
@@ -3,7 +3,7 @@ import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
     return (
-        <div className="navbar  w-10/12 mx-auto">
+        <div className="navbar  w-10/12 mx-auto mt-4 work-sans">
             <div className="navbar-start">
                 <div className="dropdown">
                     <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
@@ -12,27 +12,29 @@ const Navbar = () => {
                     <ul
                         tabIndex={0}
                         className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
-                        <NavLink>Jisan</NavLink>
                         <NavLink>Home</NavLink>
-                        <NavLink>Home</NavLink>
-                        <NavLink>Home</NavLink>
-                        <NavLink>Home</NavLink>
+                        <NavLink>Listed Books</NavLink>
+                        <NavLink>Pages to Read</NavLink>
+                        <NavLink>Blogs</NavLink>
+                        <NavLink>About Us</NavLink>
+                        <NavLink>Sign In</NavLink>
+                        <NavLink>Sign Up</NavLink>
                     </ul>
                 </div>
-                <a className="btn btn-ghost text-xl">BookVibe</a>
+                <a className="btn btn-ghost text-[#131313] text-2xl font-bold">BookVibe</a>
             </div>
             <div className="navbar-center hidden lg:flex">
-                <ul className="menu menu-horizontal px-1 flex gap-6">
-                        <NavLink>Home</NavLink>
-                        <NavLink>Home</NavLink>
-                        <NavLink>Home</NavLink>
-                        <NavLink>Home</NavLink>
-                        <NavLink>Home</NavLink>
+                <ul className="menu menu-horizontal px-1 flex gap-6 text-[131313CC] text-lg font-normal ">
+                        <NavLink className='btn bg-white border-none'>Home</NavLink>
+                        <NavLink className='btn bg-white border-none'>Listed Books</NavLink>
+                        <NavLink className='btn bg-white border-none'>Pages to Read</NavLink>
+                        <NavLink className='btn bg-white border-none'>Blogs</NavLink>
+                        <NavLink className='btn bg-white border-none'>About Us</NavLink>
                 </ul>
             </div>
-            <div className="navbar-end">
-                <button className="btn btn-primary">Sign In</button>
-                <button className="btn btn-primary">Sign Up</button>
+            <div className="navbar-end  gap-6 hidden md:flex">
+                <button className="btn bg-[#2DBD0C] text-[#FFF] text-base font-semibold px-6 py-4">Sign In</button>
+                <button className="btn bg-[#4EB2C8] text-[#FFF] text-base font-semibold px-6 py-4">Sign Up</button>
             </div>
         </div>
     );
