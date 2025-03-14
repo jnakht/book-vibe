2f10939d8d4b2f025ca2a9245f8eb608533f2efc
jnakht <jisan.cse.cu@gmail.com>
2025-03-14 16:03:23 +0600

    section title added

--- src/assets/Components/Home/Home.jsx
+++ src/assets/Components/Home/Home.jsx
@@ -1,10 +1,12 @@
 import Banner from "../Banner/Banner";
+import TitleSection from "../TitleSection/TitleSection";
 
 
 const Home = () => {
     return (
         <div className="w-10/12 mx-auto">
             <Banner></Banner>
+            <TitleSection></TitleSection>
         </div>
     );
 };
---
+++ src/assets/Components/TitleSection/TitleSection.jsx
@@ -0,0 +1,11 @@
+
+import '../../../App.css'
+const TitleSection = () => {
+    return (
+        <div className="mb-9 text-center">
+            <h3 className="text-[#131313] playfair-display text-[40px] font-bold ">Books</h3>
+        </div>
+    );
+};
+
+export default TitleSection;
