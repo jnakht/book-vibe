cecf26093cd0d111713fe5f013c4f73b0667add6
jnakht <jisan.cse.cu@gmail.com>
2025-03-15 16:44:50 +0600

    card style added with active card

--- src/assets/Components/ListedBooks/ListedBooks.jsx
+++ src/assets/Components/ListedBooks/ListedBooks.jsx
@@ -1,8 +1,11 @@
 import ListedBooksTitle from "../ListedBooksTitle/ListedBooksTitle";
 import { FaAngleDown } from "react-icons/fa";
 import '../../../App.css'
+import { Link } from "react-router-dom";
+import { useState } from "react";
 const ListedBooks = () => {
-    return (
+    const [activeCard, setActiveCard] = useState(0);
+    return (  
         <div className="w-10/12 mx-auto">
             <ListedBooksTitle></ListedBooksTitle>
             {/* dropdown menu start*/}
@@ -20,19 +23,19 @@ const ListedBooks = () => {
 
             {/* card start */}
             <div className="flex items-center -mx-4 overflow-x-auto overflow-y-hidden sm:justify-start flex-nowrap dark:bg-gray-100 dark:text-gray-800 ">
-                <a rel="noopener noreferrer" href="#" className="flex items-center flex-shrink-0 px-5 py-3 space-x-2 border-b dark:border-gray-600 dark:text-gray-600">
+                <Link onClick={() => setActiveCard(0)} className={`flex items-center flex-shrink-0 px-5 py-3 space-x-2  dark:border-gray-600 dark:text-gray-600 ${activeCard === 0 ? 'border border-b-0 rounded-t-lg ' : 'border-b'}`}>
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="w-4 h-4">
                         <path d="M19 21l-7-5-7 5V5a2 2 0 0 1 2-2h10a2 2 0 0 1 2 2z"></path>
                     </svg>
                     <span>Architecto</span>
-                </a>
-                <a rel="noopener noreferrer" href="#" className="flex items-center flex-shrink-0 px-5 py-3 space-x-2 border border-b-0 rounded-t-lg dark:border-gray-600 dark:text-gray-900">
+                </Link>
+                <Link onClick={() => setActiveCard(1)}  className={`flex items-center flex-shrink-0 px-5 py-3 space-x-2  dark:border-gray-600 dark:text-gray-600 ${activeCard === 1 ? 'border border-b-0 rounded-t-lg' : 'border-b'}`}>
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="w-4 h-4">
                         <path d="M2 3h6a4 4 0 0 1 4 4v14a3 3 0 0 0-3-3H2z"></path>
                         <path d="M22 3h-6a4 4 0 0 0-4 4v14a3 3 0 0 1 3-3h7z"></path>
                     </svg>
                     <span>Corrupti</span>
-                </a>
+                </Link>
                
             </div>
         </div>
