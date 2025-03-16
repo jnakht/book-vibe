2c43ae57c878928cf84aa2afdd0f22940621770a
jnakht <jisan.cse.cu@gmail.com>
2025-03-16 17:00:53 +0600

    read books, wishlist all done

--- src/assets/Components/Wishlist/Wishlist.jsx
+++ src/assets/Components/Wishlist/Wishlist.jsx
@@ -1,11 +1,22 @@
+import { useEffect, useState } from "react";
+import { getFromLocalStorage } from "../../../Utility/Utility";
+import ReadBooksCard from "../ReadBooksCard/ReadBooksCard";
 
 
 const Wishlist = () => {
-    return (
-        <div>
-            <h3>This is wishlist card</h3>
-        </div>
-    );
+    const [wishlist, setWishlist] = useState([]);
+        useEffect(() => {
+            const list = getFromLocalStorage('wishlist');
+            setWishlist(list);
+        } ,[])
+        return (
+            <div className="flex flex-col gap-6 mt-8 mb-[100px]">
+                {
+                    // the read books card can be reused in wishlist card
+                    wishlist.map(book => <ReadBooksCard book={book}></ReadBooksCard>)
+                }
+            </div>
+        );
 };
 
 export default Wishlist;
