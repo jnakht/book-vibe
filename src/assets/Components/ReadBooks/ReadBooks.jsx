f74c6b6a691d856cb5a9187eeb3878317722557f
jnakht <jisan.cse.cu@gmail.com>
2025-03-16 16:08:36 +0600

    read books card styling added

--- src/assets/Components/ReadBooks/ReadBooks.jsx
+++ src/assets/Components/ReadBooks/ReadBooks.jsx
@@ -10,7 +10,7 @@ const ReadBooks = () => {
         setReadBooks(list);
     } ,[])
     return (
-        <div>
+        <div className="flex flex-col gap-6">
             {
                 readBooks.map(book => <ReadBooksCard book={book}></ReadBooksCard>)
             }
--- src/assets/Components/ReadBooksCard/ReadBooksCard.jsx
+++ src/assets/Components/ReadBooksCard/ReadBooksCard.jsx
@@ -7,41 +7,42 @@ const ReadBooksCard = ({book}) => {
     const {bookId, bookName, author, image, category, publisher, rating, tags, review, totalPages, yearOfPublishing} = book;
     return (
         <div>
-            <div className="card card-side bg-base-100 shadow-sm">
-                <figure className="w-[30%] h-[230px] border-2 border-red-500">
+            <div className="card card-side bg-base-100 shadow-sm border border-solid border-[#13131326] h-[280px] p-6">
+                <figure className="w-[30%] h-[230px]  flex justify-center items-center rounded-lg">
                     <img
-                        className="h-[200px] w-[30%]"
+                        className="h-[200px] w-[90%] "
                         src={image} />
                 </figure>
-                <div className="card-body">
+                <div className=" ml-6 flex flex-col gap-2 ">
                     <h2 className="text-[#131313] playfair-display  text-2xl font-bold">{bookName}</h2>
                     <p className="text-[#131313CC] work-sans text-base font-medium">By: {author}</p>
-                    <div>
-                        <div>
+                    <div className="flex gap-8 items-center">
+                        <div className="flex gap-4 items-center">
                             <span className="text-[#131313]  work-sans text-base font-bold">Tags</span>
                             {
                                 tags.map(tag => <button className="bg-[#23BE0A0D] px-4 py-2 rounded-[30px] text-[#23BE0A] work-sans text-base font-medium ">{tag}</button>)
                             }
                         </div>
-                        <div className="flex text-[#131313CC] work-sans text-base font-normal">
+                        <div className="flex text-[#131313CC] work-sans text-base font-normal items-center gap-2">
                              <FaLocationDot />
                              <p>Year Of Publishing: {yearOfPublishing}</p>
                         </div>
                     </div>
-                    <div className="flex work-sans text-base font-normal text-[#13131399]">
-                        <div className="flex">
+                    <div className="flex work-sans text-base font-normal text-[#13131399] items-center gap-8">
+                        <div className="flex items-center gap-2">
                             <IoPeopleOutline />
                             <p>Publisher: {publisher}</p>
                         </div>
-                        <div className="flex">
+                        <div className="flex items-center gap-2">
                             <FaRegFileAlt />
                             <p>Page: {totalPages}</p>
                         </div>
                     </div>
-                    <div>
-                        <button>Category: {category}</button>
-                        <button>Rating: {rating}</button>
-                        <Link><button>View Details</button></Link>
+                    <hr className="text-[#13131399] mt-4 mb-4" />
+                    <div className="flex gap-3 items-center">
+                        <button className="bg-[#328EFF26] rounded-4xl text-[#328EFF] work-sans text-base font-normal px-5 py-2">Category: {category}</button>
+                        <button className="bg-[#FFAC331F] rounded-4xl text-[#FFAC33] work-sans text-base font-normal px-5 py-2">Rating: {rating}</button>
+                        <Link><button className="bg-[#23BE0A] rounded-4xl text-[#FFF] work-sans text-base font-normal px-5 py-2">View Details</button></Link>
                     </div>
                 </div>
             </div>
