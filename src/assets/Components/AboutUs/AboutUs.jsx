eba6e0d904630e65191aa236f89362af64ab7992
jnakht <jisan.cse.cu@gmail.com>
2025-03-17 22:36:53 +0600

    about us added

--- src/assets/Components/AboutUs/AboutUs.jsx
+++ src/assets/Components/AboutUs/AboutUs.jsx
@@ -2,8 +2,65 @@
 
 const AboutUs = () => {
     return (
-        <div>
-            <h3>This is about us</h3>
+        <div className="w-10/12 mx-auto mt-5 mb-15 ">
+            <section className="dark:bg-gray-100 dark:text-gray-800 rounded-xl">
+                <div className="container px-6 py-12 mx-auto">
+                    <div className="grid items-center gap-4 xl:grid-cols-5">
+                        <div className="max-w-2xl mx-auto my-8 space-y-4 text-center xl:col-span-2 xl:text-left">
+                            <h2 className="text-4xl font-bold">About Us</h2>
+                            <p className="dark:text-gray-600">At BookVite, we believe in the power of stories to inspire, educate, and connect people. Our mission is to create a space where book lovers can explore, discover, and share their favorite reads. With a carefully curated collection and a passion for literature, we strive to make every bookshelf richer and every reader's journey more fulfilling.</p>
+                        </div>
+                        <div className="p-6 xl:col-span-3">
+                            <div className="grid gap-4 md:grid-cols-2">
+                                <div className="grid content-center gap-4">
+                                    <div className="p-6 rounded shadow-md dark:bg-gray-50">
+                                        <p>We embrace the art of meaningful conversations and the joy of discovering new perspectives. At BookVite, we are committed to fostering a vibrant community where ideas flow freely, stories come to life, and every reader finds their next great adventure. With a passion for books and innovation, we strive to make reading more engaging, accessible, and inspiring for everyone.</p>
+                                        <div className="flex items-center mt-4 space-x-4">
+                                            <img src="https://i.ibb.co.com/4w17TBBJ/pexels-kampus-8353841.jpg" alt="" className="w-12 h-12 bg-center bg-cover rounded-full dark:bg-gray-500" />
+                                            <div>
+                                                <p className="text-lg font-semibold">Leroy Jenkins</p>
+                                                <p className="text-sm dark:text-gray-600">CTO of Company Co.</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div className="p-6 rounded shadow-md dark:bg-gray-50">
+                                        <p>At BookVite, we believe in the power of stories to connect, inspire, and transform. Our mission is to create a space where readers can explore diverse ideas, engage in meaningful discussions, and discover books that ignite their curiosity. With a commitment to innovation and community, we strive to make reading a richer and more immersive experience for all.</p>
+                                        <div className="flex items-center mt-4 space-x-4">
+                                            <img src="https://i.ibb.co.com/nMhHDGpX/pexels-artempodrez-7956488.jpg" alt="" className="w-12 h-12 bg-center bg-cover rounded-full dark:bg-gray-500" />
+                                            <div>
+                                                <p className="text-lg font-semibold">Leroy Jenkins</p>
+                                                <p className="text-sm dark:text-gray-600">CTO of Company Co.</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="grid content-center gap-4">
+                                    <div className="p-6 rounded shadow-md dark:bg-gray-50">
+                                        <p>At BookVite, we embrace the endless possibilities of knowledge and storytelling. Our platform is built for readers who seek to explore, engage, and evolve through literature. We are dedicated to fostering a vibrant community where ideas flow freely, curiosity is celebrated, and every book opens the door to new perspectives.</p>
+                                        <div className="flex items-center mt-4 space-x-4">
+                                            <img src="https://i.ibb.co.com/B544tHbP/pexels-albarracin-30797307.jpg" alt="" className="w-12 h-12 bg-center bg-cover rounded-full dark:bg-gray-500" />
+                                            <div>
+                                                <p className="text-lg font-semibold">Leroy Jenkins</p>
+                                                <p className="text-sm dark:text-gray-600">CTO of Company Co.</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div className="p-6 rounded shadow-md dark:bg-gray-50">
+                                        <p>At BookVite, we believe in the power of knowledge and the impact of shared stories. Our mission is to connect passionate readers with books that inspire, challenge, and transform. With a commitment to discovery and intellectual growth, we strive to create a space where literature fuels conversation and creativity.</p>
+                                        <div className="flex items-center mt-4 space-x-4">
+                                            <img src="https://i.ibb.co.com/5hstDJKQ/pexels-kampus-7233019.jpg" alt="" className="w-12 h-12 bg-center bg-cover rounded-full dark:bg-gray-500" />
+                                            <div>
+                                                <p className="text-lg font-semibold">Leroy Jenkins</p>
+                                                <p className="text-sm dark:text-gray-600">CTO of Company Co.</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
         </div>
     );
 };
